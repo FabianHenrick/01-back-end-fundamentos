0be98f15a14e00c740de6d1ff2784b16d199fb54
FabianHenrick <fabbianhenrick@gmail.com>
2024-03-07 17:22:33 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,6 @@
+//CommonJs => require (metodo antigo de importação)
+//const http = require('http')
+
+
+// ESModules => import/export ( metodo mais usado atualmente - necessário adcionar o type: "module") para usar o padrão ES.
+import http from "node:http"
